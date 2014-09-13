f68a32f093eb4be40fe4d9fb7b9c4bacbd708443
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2014-09-13 13:43:29 +0900

    deviseのjson API対応をconfig/application.rb での設定方式に変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   resources :notes
   resource :authentication_token, only: [:update, :destroy]
 
-  devise_for :users, controllers: {
-    sessions:      'users/sessions',
-    registrations: 'users/registrations'
-  }
-
+  devise_for :users
   root to: 'home#index'
 end
